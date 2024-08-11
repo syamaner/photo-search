448583e49b04006eb6d5e92461c27a12e2ce3d2b
syamaner <syamaner@users.noreply.github.com>
2024-08-11 16:39:05 +0100

    minor formatting of the model view

--- src/photosearch-frontend/src/components/photo-summary/photo-summary-view.tsx
+++ src/photosearch-frontend/src/components/photo-summary/photo-summary-view.tsx
@@ -16,7 +16,7 @@ export class PhotoSummaryView {
   @Prop() selectedSummary: string;
   onPhotoSelected = (_msg: string, data: PhotoSummary) => {
     this.selectedPhoto = data;
-    
+
     for (const key in data.Summaries) {
       if (data.Summaries.hasOwnProperty(key)) {
         this.availableModels.add(key);
@@ -43,14 +43,18 @@ export class PhotoSummaryView {
       return (
         <Host>
           <slot>
-            <select onInput={(event) => this.handleSelect(event)}>
-            <option value='-' selected={true}>Please Select a model.</option>
+          <form class="flex mb-4 map-container p-3" >
+            <label  class="w-1/4  align-middle pt-2 ">Select an option</label>
+            
+            <select id="models" onInput={(event) => this.handleSelect(event)} class="w-3/4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value='-' selected={true}>Please Select a model.</option>
               {[...this.availableModels].map(model => (
-                <option value={model} selected={this.selectedModel===model}>{model}  </option>
+                <option value={model} selected={this.selectedModel === model}>{model}  </option>
               ))}
             </select>
+            </form>
             <div class="p-3">
-            <article class="prose prose-lg">{this.selectedSummary}</article>
+              <p class="prose prose-lg">{this.selectedSummary}</p>
             </div>
           </slot>
         </Host>
